dcd3e7462420fde4e4ed5dd182267f2ff53cea1d
adrianaarcia <adrianaarcia@gmail.com>
2021-05-11 02:26:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,42 +3,46 @@ from vispy import app, io
 from calc import norms, normalize, normalize_multi
 from show import show_scatter, show_mesh, assign_colors
 
-#get path to file
-path_head = 'assets/average/'
-path_end = '/anysex/'
+racial_groups = ['anyrace', 'african', 'eastasian', 'european', 'southasian']
 
-filename_head = 'avg_'
-filename_end = '_anysex.obj'
+def comp_avg(sex, mcmap='hsl', scmap='viridis'):
+    #get path to file
+    path_head = 'assets/average/'
+    path_end = '/' + sex + '/'
 
-#choose face to compare
-cmp_index = 2
-dirs = ['anyrace', 'african', 'eastasian', 'european', 'southasian']
+    filename_head = 'avg_'
+    filename_end = '_' + sex + '.obj'
 
-verts = []
-faces = []
-for dir in dirs:
-    path = path_head+dir+path_end+filename_head+dir+filename_end
-    vertices, facess, normals, nothing = io.read_mesh(path)
-    verts.append(vertices)
-    faces.append(facess)
+    #initialize arrays 
+    verts = []
+    faces = []
+    for grp in racial_groups: #store vertices and faces for each face
+        path = path_head+grp+path_end+filename_head+grp+filename_end
+        vertices, facess, normals, nothing = io.read_mesh(path)
+        verts.append(vertices)
+        faces.append(facess)
 
-nrms = [norms(verts[0], vert) for vert in verts]
-normalized_single = [normalize(x) for x in nrms]
-normalized_all = normalize_multi(nrms)
+    #calculate the norm of each face with respect to 'anyrace'
+    nrms = [norms(verts[0], vert) for vert in verts]
+    normalized_single = [normalize(x) for x in nrms] #normalize magnitude across self
+    normalized_all = normalize_multi(nrms) #normalize magnitude across all faces
 
-#get colors
-nall_arr = np.array(normalized_all[cmp_index])
-nrms_arr = np.array(nrms[cmp_index])
-vs = verts[cmp_index]
-fs = faces[cmp_index]
+    for i in range(1, 4): #skip 'anyrace' as we don't care to compare it to itself
+        #get colors
+        nall_arr = np.array(normalized_single[i])
+        nrms_arr = np.array(nrms[i])
+        
+        sc = assign_colors(nall_arr, nrms_arr, scmap)
+        mc = assign_colors(nall_arr, nrms_arr, mcmap)
 
-sc = assign_colors(nall_arr, nrms_arr)
-mc = assign_colors(nall_arr, nrms_arr)
+        #display results
+        vs = verts[i]
+        fs = faces[i]
 
-#display results
-show_scatter(vs, sc)
-show_mesh(vs, fs, mc)
+        #show_scatter(vs, sc)
+        show_mesh(vs, fs, mc)
 
+comp_avg('anysex')
 if __name__ == '__main__':
     import sys
     if sys.flags.interactive != 1:
