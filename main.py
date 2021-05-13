5226394aa51099c47f1d26aa95fa1ef1fc70b2b0
adrianaarcia <adrianaarcia@gmail.com>
2021-05-13 00:19:56 -0400

    update

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import numpy as np
-from vispy import app, io
+from vispy import app, io, scene
+import vispy
 from calc import norms, normalize, normalize_multi
 from show import show_scatter, show_mesh, assign_colors
 
 racial_groups = ['anyrace', 'african', 'eastasian', 'european', 'southasian']
 
-def comp_avg(sex, mcmap='hsl', scmap='viridis'):
+def comp_avg(sex, mcmap='blues', scmap='viridis'):
+    
     #get path to file
     path_head = 'assets/average/'
     path_end = '/' + sex + '/'
 
     filename_head = 'avg_'
-    filename_end = '_' + sex + '.obj'
+    filename_end = '_' + sex
 
     #initialize arrays 
     verts = []
     faces = []
     for grp in racial_groups: #store vertices and faces for each face
-        path = path_head+grp+path_end+filename_head+grp+filename_end
+        path = path_head+grp+path_end+filename_head+grp+filename_end+'.obj'
         vertices, facess, normals, nothing = io.read_mesh(path)
         verts.append(vertices)
         faces.append(facess)
@@ -27,7 +29,10 @@ def comp_avg(sex, mcmap='hsl', scmap='viridis'):
     normalized_single = [normalize(x) for x in nrms] #normalize magnitude across self
     normalized_all = normalize_multi(nrms) #normalize magnitude across all faces
 
-    for i in range(1, 4): #skip 'anyrace' as we don't care to compare it to itself
+    # nall_arr = np.array(normalized_single[i])
+    # nrms_arr = np.array(nrms[i])
+
+    for i in range(1, len(racial_groups)): #skip 'anyrace' as we don't care to compare it to itself
         #get colors
         nall_arr = np.array(normalized_single[i])
         nrms_arr = np.array(nrms[i])
@@ -39,10 +44,14 @@ def comp_avg(sex, mcmap='hsl', scmap='viridis'):
         vs = verts[i]
         fs = faces[i]
 
-        #show_scatter(vs, sc)
-        show_mesh(vs, fs, mc)
+        rg = racial_groups[i]
+        dest_path = 'results/average/' + rg + path_end +  filename_head + rg +'_'+sex
+
+        show_scatter(vs, sc, name)
+        show_mesh(vs, fs, mc, name)
 
 comp_avg('anysex')
+
 if __name__ == '__main__':
     import sys
     if sys.flags.interactive != 1:
--- show.py
+++ show.py
@@ -1,5 +1,5 @@
 import numpy as np
-from vispy import scene, visuals, color
+from vispy import scene, visuals, color, io
 
 def assign_colors(normalized_arr, nrms_arr, cmap='husl'):      
     colors = color.get_colormap(cmap).map(normalized_arr).reshape(nrms_arr.shape + (-1,))
@@ -8,26 +8,35 @@ def assign_colors(normalized_arr, nrms_arr, cmap='husl'):
 
     return colors
 
-def viewbox():
-    canvas = scene.SceneCanvas(keys='interactive', show=True)
+def viewbox(canvas):
     vb = canvas.central_widget.add_view()
     vb.camera = scene.TurntableCamera(up='+y', distance=500)
     vb.camera.rect = (-10, -10, 20, 20)
 
     return vb
 
-def show_scatter(verts, colors):
-    view = viewbox()
+def show_scatter(verts, colors, name):
+    s = scene.canvas.SceneCanvas(keys='interactive', show=True)
+    view = viewbox(s)
+   
 
     Scatter3D = scene.visuals.create_visual_node(visuals.MarkersVisual)
     p1 = Scatter3D(parent=view.scene)
     p1.set_gl_state('translucent', blend=True, depth_test=True)
     p1.set_data(verts, symbol='o', size=5, edge_width=0.2, face_color=colors)
+    
+    img = s.render()
+    io.write_png('results/'+name + "_verts.png", img)
 
-def show_mesh(vs, fs, colors):
-    view = viewbox()
+
+def show_mesh(vs, fs, colors, name):
+    s = scene.canvas.SceneCanvas(keys='interactive', show=True)
+    view = viewbox(s)
 
     mesh = scene.visuals.Mesh(vertices=vs, faces=fs, shading='smooth', vertex_colors=colors)
     mesh.set_gl_state('translucent', depth_test=True, cull_face=True)
 
     view.add(mesh)
+
+    img = s.render()
+    io.write_png('results/'+name + "_mesh.png",img)
